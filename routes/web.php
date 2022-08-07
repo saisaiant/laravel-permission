38ee44923aa2d30a14252d3561273e560102e0f3
kosai <saisaiant@gmail.com>
2022-08-07 14:30:18 +0630

    add admin layout & role permission controller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\RoleController;
+use App\Http\Controllers\Admin\IndexController;
+use App\Http\Controllers\Admin\PermissionController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,5 +23,13 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
+
+Route::middleware(['auth', 'role:admin'])->name('admin.')->prefix('admin')->group(function() {
+    Route::get('/', [IndexController::class, 'index'])->name('index');
+    Route::resource('/roles', RoleController::class);
+    Route::resource('/permissions', PermissionController::class);
+});
+
+
 
 require __DIR__.'/auth.php';
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+    // darkMode: 'class',
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
@@ -10,11 +11,16 @@ module.exports = {
 
     theme: {
         extend: {
+            // screens: {
+            //     'light-mode': { raw: '(prefers-color-scheme: light)' },
+            //   },
+            screens: {
+                'dark-mode': { raw: '(prefers-color-scheme: dark)' },
+            },
             fontFamily: {
                 sans: ['Nunito', ...defaultTheme.fontFamily.sans],
-            },
-        },
+            },            
+        },       
     },
-
     plugins: [require('@tailwindcss/forms')],
 };
